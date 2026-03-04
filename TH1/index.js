3012fbec892d1187e78f3339c9767915260a79ef
taimo <chickenvn003@gmail.com>
2026-03-04 12:33:46 +0700

    TH1 Basic HTTP server

--- TH1/index.js
+++ TH1/index.js
@@ -7,6 +7,7 @@ const server = http.createServer((req, res) => {
     res.end('Xin chào Node.js');
 });
 
+
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
 });
