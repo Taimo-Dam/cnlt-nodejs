3c0d1c18470a8e4c805c8c954c8a549245b4631a
taimo <chickenvn003@gmail.com>
2026-03-05 15:45:20 +0700

    Cấu hình route trang

--- TH1/index.js
+++ TH1/index.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Express')
-})
+    res.sendFile(path.join(__dirname, 'home.html'));
+});
 
-app.listen(3000, () =>{
-    console.log("I Love you 3000")
-})
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server chạy tại http://localhost:3000/');
+});
