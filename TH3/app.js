e6ce05d83451bb9e275ea8e6727ba031ee350273
Taimo <chickenvn003@gmail.com>
2026-03-25 11:35:59 +0700

    Robo3T and Mongoose

--- TH3/app.js
+++ TH3/app.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/myprojectdb")
+mongoose.connect("mongodb://localhost:27017/meandyoudb")
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
 
